48611c0be08b5e5edf710b209da25e1e418b3bd1
DESKTOP-8433VK4\xuwei <ghostlovegg@gmail.com>
2017-06-26 11:11:28 +0800

    no message

---
+++ Game/Source/Game/OpenDoor.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "Engine/World.h"
+#include "GameFramework/Character.h"
+#include "Engine/TriggerVolume.h"
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GAME_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void OpenDoor();
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	float angle = 90.f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	ACharacter* OpenEr;
+	
+		
+	
+};
